14b13f102fceddcab0c5b56bb06bc709ed2a0e29
Gabe Grand <grandg@mit.edu>
2023-11-20 19:54:58 +0000

    Fix import bug in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='hfppl',
-    version='0.1.0',    
+    version='0.1.0',
     description='Probabilistic programming with HuggingFace Transformer models',
     url='https://github.com/probcomp/hfppl',
     author='Alex Lew',
     author_email='alexlew@mit.edu',
     license='MIT',
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     install_requires=['torch',
                       'numpy',
                       'transformers==4.30',
